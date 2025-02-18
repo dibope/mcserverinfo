3ad002c27c2baf40800e2d2a11ee9cc6288ebce5
dibope <zmwoxnqp@gmail.com>
2025-02-18 22:40:29 +0530

    Update config.py

--- .github/workflows/config.py
+++ .github/workflows/config.py
@@ -1,2 +1,9 @@
+# Change these with dummy ones
 API_BASE_URL = "https://api.com"
+'''
+Change path accordingly in file and use these variables bot.py
+listURL = {API_BASE_URL}/servers/list
+inspectURL = {API_BASE_URL}/servers/inspect/{server_id}
+logsURL = {API_BASE_URL}/servers/logs/{server_id}
+'''
 BOT_TOKEN = "DISCORD_BOT_TOKEN"
